81b5b66c29a69d361946c80889ea2130c3c509e9
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-14 11:44:17 +0800

    Skillvine - add staff user management page with search and role filtering

--- staff/urls.py
+++ staff/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import staff_dashboard
+from .views import staff_dashboard, staff_users
 
 urlpatterns = [
     path("", staff_dashboard, name="staff_dashboard"),
+    path("users/", staff_users, name="staff_users"),
 ]
