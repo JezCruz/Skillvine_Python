760395df4169e51ea6486e99ae95bdbc036e43ac
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-27 19:58:44 +0800

    Skillvine – update version API

--- api/views.py
+++ api/views.py
@@ -20,15 +20,16 @@ class EmailOrUsernameLoginView(TokenObtainPairView):
     serializer_class = EmailOrUsernameTokenObtainPairSerializer
 
 
-@api_view(["GET"])
+@api_view(['GET'])
 def app_version(request):
     return Response({
-        "latest_version": "1.0.1",
-        "apk_url": "http://skillvines.com/downloads/apk/skillvine-v1.0.1.apk",
+        "latest_version": "1.0.2",
+        "apk_url": "http://skillvines.com/downloads/apk/skillvine-v1.0.2.apk",
         "force_update": False,
         "notes": [
-            "Added auto-refresh",
-            "Added in-app update checker"
+            "Added update system",
+            "Improved UI",
+            "Bug fixes"
         ]
     })
 
