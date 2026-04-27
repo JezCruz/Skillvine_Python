150a49f66aeb9ae09b68dbedeeb37cc0958018e9
JezCruz <132325622+JezCruz@users.noreply.github.com>
2026-04-27 19:56:19 +0800

    Revert "Skillvine – add updates screen"
    This reverts commit f6e52f40c3a4cfb0d3cb73d2cdc93400c6519c8a.

--- api/views.py
+++ api/views.py
@@ -23,12 +23,12 @@ class EmailOrUsernameLoginView(TokenObtainPairView):
 @api_view(["GET"])
 def app_version(request):
     return Response({
-        "latest_version": "1.0.2",
-        "apk_url": "http://skillvines.com/downloads/apk/skillvine-v1.0.2.apk",
+        "latest_version": "1.0.1",
+        "apk_url": "http://skillvines.com/downloads/apk/skillvine-v1.0.1.apk",
+        "force_update": False,
         "notes": [
-            "Added update system",
-            "Improved UI",
-            "Bug fixes"
+            "Added auto-refresh",
+            "Added in-app update checker"
         ]
     })
 
